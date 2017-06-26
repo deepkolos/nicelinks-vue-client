fcff32436fc2b8da3c1bc4dcd950224e81a57f2a
jade.yang <yunjeff@163.com>
2017-06-26 20:01:16 +0800

    feat(view): update page title based on current content😊

--- src/router/beforeEachHooks.js
+++ src/router/beforeEachHooks.js
@@ -1,10 +1,16 @@
 /*
 * 路由跳转权限控制
 */
+import Vue from 'vue'
 import { $auth } from 'helper'
+
 export default {
   // 检查登录态
   checkLoginAuth (to, from, next) {
+    if (to.meta.title && to.meta.title[Vue.config.lang]) {
+      document.title = to.meta.title[Vue.config.lang]
+    }
+
     if (to.meta && !to.meta.needAuth) {
       next()
     } else {
