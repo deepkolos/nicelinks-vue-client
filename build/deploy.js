8d678c71f8e4cf49f6432429ffedc20b2e2fce1e
jade.yang <yunjeff@163.com>
2017-07-07 19:21:41 +0800

    feat(shell): update the shell about deploy 😅

--- build/deploy.js
+++ build/deploy.js
@@ -2,11 +2,26 @@ var path = require('path')
 var shell = require('shelljs')
 
 let sourcePath = path.resolve(__dirname, '../dist/*')
-let targetPath = './../nice-koa2-api/public'
+// let targetPath = './../nice-koa2-api/public'
+// shell.rm('-rf', `${targetPath}/static`)
+let targetPath = './../nicelinks/'
 shell.rm('-rf', `${targetPath}/static`)
 shell.mkdir('-p', targetPath)
 shell.config.silent = true
 shell.cp('-R', sourcePath, targetPath)
+
+shell.cd(targetPath)
+
+if (!which('git')) {
+  echo('Sorry, this script requires git')
+  exit(1)
+}
+
+if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
+  shell.echo('Error: Git commit failed')
+  shell.exit(1)
+}
+
 shell.config.silent = false
 
 console.log('Deploy Has been completed.')
