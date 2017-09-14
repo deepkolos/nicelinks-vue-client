a9da11d7d41c9b31b66048ac95618cb2f603b66e
jeffjade <jeffygisgreat@gmail.com>
2017-09-15 00:00:30 +0800

    feat(default): chnage the default config

--- src/config/default.js
+++ src/config/default.js
@@ -1,99 +1,161 @@
 export default [
   {
-    '_id': '58eb41c15420d209bb343725',
-    'urlPath': 'jeffjade.com',
-    'title': '晚晴幽草轩',
-    'classify': '0',
-    'tags': 'Frontend;Programmer;Backend',
+    '_id': '59ba84753df6765c75b7791b',
+    'urlPath': 'http://www.feekr.com/',
+    'title': 'Feekr - 与友分享温暖的旅行回忆',
+    'classify': '2',
+    'theme': 'Travel',
+    'userId': '59b9368bba0f986d7e072165',
+    'createdBy': 'admin',
     '__v': 0,
-    'like_ip_arr': {
-      '053ccd3883941566cee519786910e77e': true,
-      'e1f99a0d7af798e825f8cd4cc491f6d3': true
-    },
-    'updated': '2017-04-10T08:26:41.990Z',
-    'created': '2017-04-10T08:26:41.989Z',
-    'dislike_arr': {
-      '8e041bb79cc45ccdc902ca4db0381df9': true
-    },
-    'dislike': 1,
-    'like_arr': {
-      'undefined': true,
-      '4319e7b5eb4dc19e20e499c4877673d2': false,
-      '8e041bb79cc45ccdc902ca4db0381df9': true
-    },
-    'like': 2,
-    'desc': '追求极致、极客'
+    'updated': '2017-09-14T13:30:29.885Z',
+    'created': '2017-09-14T13:30:29.885Z',
+    'active': true,
+    'isDislikes': false,
+    'isLikes': false,
+    'dislikes': 0,
+    'tags': [
+      '官网',
+      '发现',
+      '旅行'
+    ],
+    'likes': 1,
+    'desc': 'Feekr为你提供真实感兴趣的旅游分享，景点攻略，有真实的用户旅游经历，漂亮的旅游图片。Feekr，与友分享温暖的旅行回忆。'
   },
   {
-    '_id': '58eb762c5420d209bb343726',
-    'urlPath': 'http://nicejade.github.io/',
+    '_id': '59ba84083df6765c75b77919',
+    'urlPath': 'https://500px.com/',
+    'title': 'The Premier Photography Community',
+    'classify': '1',
+    'theme': 'Picture',
+    'userId': '59b9368bba0f986d7e072165',
+    'createdBy': 'admin',
+    '__v': 0,
+    'updated': '2017-09-14T13:28:40.560Z',
+    'created': '2017-09-14T13:28:40.560Z',
+    'active': true,
+    'isDislikes': false,
+    'isLikes': false,
+    'dislikes': 0,
+    'tags': [
+      '官网',
+      '发现',
+      '美图'
+    ],
+    'likes': 0,
+    'desc': `Find your home among the world's best photographers.`
+  },
+  {
+    '_id': '59ba82953df6765c75b77915',
+    'urlPath': 'https://www.zhihu.com/',
+    'title': '知乎 - 发现更大的世界',
+    'classify': '1',
+    'theme': 'Forum',
+    'userId': '59b9368bba0f986d7e072165',
+    'createdBy': 'admin',
+    '__v': 0,
+    'updated': '2017-09-14T13:22:29.529Z',
+    'created': '2017-09-14T13:22:29.529Z',
+    'active': true,
+    'isDislikes': false,
+    'isLikes': false,
+    'dislikes': 0,
+    'tags': [
+      '官网',
+      '发现',
+      '论坛'
+    ],
+    'likes': 0,
+    'desc': '中文互联网最大的知识平台，帮助人们便捷地分享彼此的知识、经验和见解。'
+  },
+  {
+    '_id': '59ba81e73df6765c75b77913',
+    'urlPath': 'http://www.dji.com/',
+    'title': 'DJI - The Future Of Possible',
+    'classify': '1',
+    'theme': 'OfficialWebsite',
+    'userId': '59b9368bba0f986d7e072165',
+    'createdBy': 'admin',
+    '__v': 0,
+    'updated': '2017-09-14T13:19:35.679Z',
+    'created': '2017-09-14T13:19:35.679Z',
+    'active': true,
+    'isDislikes': false,
+    'isLikes': false,
+    'dislikes': 0,
+    'tags': [
+      '官网',
+      '无人机',
+      '高科技'
+    ],
+    'likes': 1,
+    'desc': 'As the market leader in easy-to-fly drones and aerial photography systems, DJI quadcopters like the Phantom are the standard in consumer drone technology.'
+  },
+  {
+    '_id': '59ba80d93df6765c75b77911',
+    'urlPath': 'https://github.com/',
+    'title': 'Github - Built for developers',
+    'classify': '0',
+    'theme': 'Program',
+    'userId': '59b9368bba0f986d7e072165',
+    'createdBy': 'admin',
+    '__v': 0,
+    'updated': '2017-09-14T13:15:05.712Z',
+    'created': '2017-09-14T13:15:05.712Z',
+    'active': true,
+    'isDislikes': false,
+    'isLikes': false,
+    'dislikes': 0,
+    'tags': [
+      '程序员',
+      'Github'
+    ],
+    'likes': 2,
+    'desc': 'GitHub is where people build software. More than 24 million people use GitHub to discover, fork, and contribute to over 66 million projects.'
+  },
+  {
+    '_id': '59ba7f5d3df6765c75b7790f',
+    'urlPath': 'https://blog.lovejade.cn/',
     'title': '天意人间舫',
     'classify': '0',
-    'tags': 'Programmer;Frontend;Backend',
+    'theme': 'Program',
+    'userId': '59b9368bba0f986d7e072165',
+    'createdBy': 'admin',
     '__v': 0,
-    'like_ip_arr': {
-      '053ccd3883941566cee519786910e77e': true,
-      '2540cf7f79e40d79316645d58f93715a': true
-    },
-    'updated': '2017-04-10T12:10:20.179Z',
-    'created': '2017-04-10T12:10:20.179Z',
-    'dislike': 0,
-    'like_arr': {
-      '2540cf7f79e40d79316645d58f93715a': true
-    },
-    'like': 1,
-    'desc': '绿叶般华丽存在'
+    'updated': '2017-09-14T13:08:45.947Z',
+    'created': '2017-09-14T13:08:45.947Z',
+    'active': true,
+    'isDislikes': false,
+    'isLikes': false,
+    'dislikes': 0,
+    'tags': [
+      '前端开发',
+      '个人博客'
+    ],
+    'likes': 1,
+    'desc': '辅助晚晴幽草轩の华丽存在。'
   },
   {
-    '_id': '58ecaa8e8cdb573cd13604c0',
-    'urlPath': 'http://www.ruanyifeng.com/blog/',
-    'title': '阮一峰的个人网站',
+    '_id': '59ba7b003df6765c75b7790c',
+    'urlPath': 'https://jeffjade.com',
+    'title': '晚晴幽草轩',
     'classify': '0',
-    'tags': 'Programmer;Frontend',
+    'theme': 'Program',
+    'userId': '59b9368bba0f986d7e072165',
+    'createdBy': 'admin',
     '__v': 0,
-    'like_ip_arr': {
-      '2540cf7f79e40d79316645d58f93715a': true
-    },
-    'updated': '2017-04-11T10:06:06.167Z',
-    'created': '2017-04-11T10:06:06.167Z',
-    'dislike_arr': {
-      '2540cf7f79e40d79316645d58f93715a': true
-    },
-    'dislike': 1,
-    'like_arr': {
-      '4319e7b5eb4dc19e20e499c4877673d2': true,
-      '2540cf7f79e40d79316645d58f93715a': true
-    },
-    'like': 2,
-    'desc': '阮一峰的个人网站'
-  },
-  {
-    '_id': '58ef7c3399482a068b88e77a',
-    'urlPath': 'http://www.williamlong.info/',
-    'title': '月光博客',
-    'classify': '3',
-    'tags': 'Technology',
-    '__v': 0,
-    'updated': '2017-04-13T13:25:07.162Z',
-    'created': '2017-04-13T13:25:07.161Z',
-    'dislike': 0,
-    'like_arr': {
-      'undefined': true
-    },
-    'like': 1,
-    'desc': '月光博客，是一个专注于互联网、搜索引擎、社会化网络、IT技术、谷歌地图、软件应用等领域的原创IT科技博客，作者龙威廉（williamlong）。'
-  },
-  {
-    '_id': '58ef7d2799482a068b88e77b',
-    'urlPath': 'https://www.huxiu.com/',
-    'title': '虎嗅网',
-    'classify': '3',
-    'tags': 'Technology',
-    '__v': 0,
-    'updated': '2017-04-13T13:29:11.580Z',
-    'created': '2017-04-13T13:29:11.580Z',
-    'dislike': 0,
-    'like': 0,
-    'desc': '聚合优质的创新信息与人群，捕获精选|深度|犀利的商业科技资讯。在虎嗅，不错过互联网的每个重要时刻。'
+    'updated': '2017-09-14T12:50:08.409Z',
+    'created': '2017-09-14T12:50:08.409Z',
+    'active': true,
+    'isDislikes': false,
+    'isLikes': false,
+    'dislikes': 0,
+    'tags': [
+      '前端开发',
+      '程序员',
+      'Node'
+    ],
+    'likes': 3,
+    'desc': '个人博客：专注web前端开发和效率工具；也记载关于音乐/电影/旅行等轶事，同时记录下经验总结和人生思考；'
   }
 ]
